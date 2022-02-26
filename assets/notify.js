f01b53b63cca75b7a448e23913b1a288ae746070
baby-thailand <swisscardiac@gmail.com>
2022-02-26 07:48:35 +0700

    ban api

--- assets/notify.js
+++ assets/notify.js
@@ -179,7 +179,7 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=';
+            window.location.href = './thankyou.html?fb_pixel_id=1191384564936792';
         });
         
     }
