b2dabd5468b2116a308958112dbaf5aaa6efb5f4
thailand-todays <100329584+thailand-todays@users.noreply.github.com>
2022-03-31 11:07:40 +0700

    Update notify.js

--- assets/notify.js
+++ assets/notify.js
@@ -179,7 +179,7 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=5218561174873860';
+            window.location.href = './thankyou.html?fb_pixel_id=';
         });
         
     }
